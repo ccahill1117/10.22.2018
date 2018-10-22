1676c82fcc17365c3e95247e67eb2c94117c8e0f
Christopher Cahill and Paige Williams <paige@>
2018-10-22 16:26:56 -0700

    append to li element.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   $("form#listEnter").submit(function(event) {
-    // function splitString(stringToSplit, separator){
-    //   return stringToSplit.split(separator);
-    // }
+    
     function splitString(stringToSplit, separator) {
         return (stringToSplit.split(separator).sort());
           // debugger
@@ -14,9 +12,8 @@ $(document).ready(function() {
 
     var split1 = splitString(inputGroc, comma);
 
-    var split2 = split1.map(function(splitP) {
-      return uppercase(splitP);
-    });
+
+
 
 
     function uppercase(str) {
@@ -24,17 +21,23 @@ $(document).ready(function() {
       for(var x = 0; x < split1.length; x++) {
         newarray.push(split1[x].charAt(0).toUpperCase()+split1[x].slice(1));
 
-
       }
 
-      return newarray.join(' ');
+      return newarray;
     }
+
+    // var split2 = split1.map(function(splitP) {
+    //   return uppercase(splitP);
+    // });
+
+      var split2 = uppercase(split1);
+      // var split3 = [split2];
+
       console.log(split2);
 
-
-
-
-
+      split2.forEach(function(item) {
+        $("#outputList").append('<li>' + item + '</li>')
+      });
 
 
 
